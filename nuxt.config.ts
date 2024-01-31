03fad3e26e3b9af9027e762800d62e1ed61f3804
Victory Ndu <victor5ndu@gmail.com>
2024-01-31 20:56:14 +0100

    Add navbar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+   modules: [
+    '@pinia/nuxt',
+    ['@nuxtjs/google-fonts', {
+        families: {
+          Roboto: true,
+          Inter: [400, 700],
+          Lato: [100, 300],
+          Raleway: {
+            wght: [100, 400],
+            ital: [100]
+          }
+        }
+    }],
+  ],
+  css: ['~/assets/css/main.css'],
+   postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="fixed right-3">
+      <MenuBar />
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+<style>
+* {
+  font-family: "Inter", sans-serif;
+}
+</style>
+
