2a10291056380f5f0ac823767463c5ec006ef2e3
Victory Ndu <victor5ndu@gmail.com>
2024-02-07 23:21:11 +0100

    Add more services section

--- pages/index.vue
+++ pages/index.vue
@@ -36,7 +36,7 @@
     </section>
 
     <section>
-      <div class="py-[10rem]">
+      <div class="py-[5rem]">
         <div class="container mx-auto">
           <div class="grid grid-cols-2 gap-4 justify-items-stretch">
             <div class="flex flex-col">
@@ -47,7 +47,7 @@
                 </h2>
                 <p class="text-xl mt-6">
                   If you are looking at blank cassettes on the web, you may be
-                  very <br/>confused at the difference in price.
+                  very <br />confused at the difference in price.
                 </p>
                 <p class="mt-5">
                   <img src="../assets/svg/move-right (1).svg" />
@@ -57,15 +57,20 @@
               <div class="justify-self-end mt-10">
                 <div class="relative">
                   <img src="../assets/furnitureImg1.jpg" class="rounded" />
-                  <div class="p-3 bg-white absolute z-40 bottom-0 w-6/12 rounded">
+                  <div
+                    class="p-3 bg-white absolute z-40 bottom-0 w-6/12 rounded"
+                  >
                     <p class="text-[#F5CA56] text-xl">SMART KITCHEN SETUP</p>
                     <p class="font-bold text-xl mt-4">
-                      Smart Kitchen Setup with <br /> Vintage Style
+                      Smart Kitchen Setup with <br />
+                      Vintage Style
                     </p>
                     <p class="mt-8 text-xl">
                       <button type="submit" class="flex items-center hover-me">
-                        <span class="me-3">View Details</span>
-                       <img src="../assets/svg/move-right-yellow.svg" />
+                        <span class="me-3 hover:text-[#F5CA56]"
+                          >View Details</span
+                        >
+                        <img src="../assets/svg/move-right-yellow.svg" />
                       </button>
                     </p>
                   </div>
@@ -88,40 +93,160 @@
               </div>
             </div>
 
-            <div>
-               <div class="">
+            <div class="flex content-center flex-col ms-0 lg:ms-5">
+              <div class="">
                 <div class="relative">
                   <img src="../assets/furnitureImg2.jpg" class="rounded" />
-                  <div class="p-3 bg-white absolute z-40 bottom-0 w-6/12 rounded">
+                  <div
+                    class="p-3 bg-white absolute z-40 bottom-0 w-6/12 rounded"
+                  >
                     <p class="text-[#F5CA56] text-xl">SMART KITCHEN SETUP</p>
                     <p class="font-bold text-xl mt-4">
-                      Smart Kitchen Setup with <br /> Vintage Style
+                      Smart Kitchen Setup with <br />
+                      Vintage Style
                     </p>
                     <p class="mt-8 text-xl">
                       <button type="submit" class="flex items-center hover-me">
-                        <span class="me-3">View Details</span>
-                       <img src="../assets/svg/move-right-yellow.svg" />
+                        <span class="me-3 hover:text-[#F5CA56]"
+                          >View Details</span
+                        >
+                        <img src="../assets/svg/move-right-yellow.svg" />
                       </button>
                     </p>
                   </div>
                 </div>
               </div>
-               <div class="mt-5">
+              <div class="mt-5">
                 <div class="relative">
                   <img src="../assets/furnitureImg3.jpg" class="rounded" />
-                  <div class="p-3 bg-white absolute z-40 bottom-0 w-6/12 rounded">
+                  <div
+                    class="p-3 bg-white absolute z-40 bottom-0 w-6/12 rounded"
+                  >
                     <p class="text-[#F5CA56] text-xl">SMART KITCHEN SETUP</p>
                     <p class="font-bold text-xl mt-4">
-                      Smart Kitchen Setup with <br /> Vintage Style
+                      Smart Kitchen Setup with <br />
+                      Vintage Style
                     </p>
                     <p class="mt-8 text-xl">
                       <button type="submit" class="flex items-center hover-me">
-                        <span class="me-3">View Details</span>
-                       <img src="../assets/svg/move-right-yellow.svg" />
+                        <span class="me-3 hover:text-[#F5CA56]"
+                          >View Details</span
+                        >
+                        <img src="../assets/svg/move-right-yellow.svg" />
                       </button>
                     </p>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="py-[3rem]">
+        <div class="container mx-auto">
+          <div class="grid grid-cols-1 gap-4 justify-items-stretch">
+            <div class="flex flex-col">
+              <div class="justify-self-start">
+                <h2 class="text-5xl font-extrabold">
+                  We Are <span class="text-[#F5CA56]">Loaft</span>
+                  <br class="d-none lg:d-block" />Top Services that we offer
+                </h2>
+                <p class="text-xl mt-6">
+                  If you are looking at blank cassettes on the web, you may be
+                  very <br />confused at the difference in price.
+                </p>
+                <p class="mt-5">
+                  <img
+                    src="../assets/svg/move-right (1).svg"
+                    class="hover-me"
+                  />
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="grid grid-cols-2 gap-4 justify-items-stretch mt-3">
+            <div class="flex">
+              <img src="../assets/service1.jpg" class="self-center" />
+            </div>
+            <div class="flex flex-col">
+              <div
+                class="w-3/4 my-6 border border-[#f3f3f3] p-3 border-t-0 border-s-0 border-e-0"
+              >
+                <h3 class="font-extrabold text-3xl">Smart Kitchen Setup</h3>
+                <p class="text-[#8C8C8C]">
+                  If you are looking at blank cassettes on the web, you may be
+                  very confused at the difference in price. You may see some for
+                  as low as $.17 each. You may be saying to yourself.
+                </p>
+
+                <p class="mt-8 text-xl">
+                  <button type="submit" class="flex items-center hover-me">
+                    <span class="me-3 hover:text-[#F5CA56]">View Details</span>
+                    <img src="../assets/svg/move-right-yellow.svg" />
+                  </button>
+                </p>
+              </div>
+              <div
+                class="w-3/4 ms-0 lg:ms-8 my-6 border border-[#f3f3f3] p-3 border-t-0 border-b-0 border-s-0 border-e-0"
+              >
+                <h3 class="font-extrabold text-3xl">Smart Kitchen Setup</h3>
+                <p class="text-[#8C8C8C]">
+                  If you are looking at blank cassettes on the web, you may be
+                  very confused at the difference in price. You may see some for
+                  as low as $.17 each. You may be saying to yourself.
+                </p>
+
+                <p class="mt-8 text-xl">
+                  <button type="submit" class="flex items-center hover-me">
+                    <span class="me-3 hover:text-[#F5CA56]">View Details</span>
+                    <img src="../assets/svg/move-right-yellow.svg" />
+                  </button>
+                </p>
+              </div>
+              <div
+                class="w-3/4 my-6 border border-[#f3f3f3] p-3 border-b-0 border-s-0 border-e-0"
+              >
+                <h3 class="font-extrabold text-3xl">Smart Kitchen Setup</h3>
+                <p class="text-[#8C8C8C]">
+                  If you are looking at blank cassettes on the web, you may be
+                  very confused at the difference in price. You may see some for
+                  as low as $.17 each. You may be saying to yourself.
+                </p>
+
+                <p class="mt-8 text-xl">
+                  <button type="submit" class="flex items-center hover-me">
+                    <span class="me-3 hover:text-[#F5CA56]">View Details</span>
+                    <img src="../assets/svg/move-right-yellow.svg" />
+                  </button>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="py-[5rem]">
+        <div class="container mx-auto">
+          <div class="grid grid-cols-2 gap-4 justify-items-stretch">
+            <div class="flex flex-col">
+              <div class="justify-self-start">
+                <h2 class="text-5xl font-extrabold">
+                  We Are <span class="text-[#F5CA56]">Loaft</span> Here Are Some
+                  Of Our <span class="text-[#F5CA56]">Latest Works</span>
+                </h2>
+                <p class="text-xl mt-6">
+                  If you are looking at blank cassettes on the web, you may be
+                  very <br />confused at the difference in price.
+                </p>
+                <p class="mt-5">
+                  <img src="../assets/svg/move-right (1).svg" />
+                </p>
               </div>
             </div>
           </div>
