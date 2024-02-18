955c81a24fc658f3a2696e862f4027515eba9979
Victory Ndu <victor5ndu@gmail.com>
2024-02-18 10:06:52 +0100

    Complete home page

---
+++ components/Footer.vue
@@ -0,0 +1,58 @@
+<template>
+<footer class="bg-black">
+      <div class="pt-[4rem] pb-[3rem]">
+        <div class="container mx-auto px-3">
+          <div
+            class="grid grid-cols-1 lg:grid-cols-3 gap-5 justify-items-stretch"
+          >
+            <div>
+              <h3 class="text-white text-3xl font-extrabold">About Loaft</h3>
+              <p class="text-gray-300 mt-4">
+                Our commitment to customer satisfaction extends beyond the sale.
+                With our dedicated team of furniture experts, we're here to
+                assist you every step of the way, from browsing our extensive
+                inventory to delivery and beyond.
+              </p>
+              <p class="text-gray-300 mt-6">
+                Copyright&copy;{{ date }}. Made by
+                <span class="text-[#F5CA56] font-extrabold">Victory Ndu</span>
+              </p>
+            </div>
+            <div>
+              <h3 class="text-white text-3xl font-extrabold">Newsletter</h3>
+              <p class="text-gray-300 mt-4">
+                Stay Updated with our latest trend
+              </p>
+              <form class="relative mt-2">
+                <input
+                  class="required:border-red-500 p-3 w-[95%] focus:outline-none border-none"
+                  placeholder="Enter your email address"
+                  type="email"
+                  required
+                />
+                <button
+                  class="bg-[#F5CA56] py-3 px-3 mt-2 absolute right-0 bottom-0"
+                >
+                  <img src="../assets/svg/caret-right.svg" />
+                </button>
+              </form>
+            </div>
+            <div class="flex flex-col items-start">
+              <h3 class="text-white text-3xl font-extrabold">Follow Me</h3>
+              <p class="text-gray-300 mt-4">Let us be social</p>
+              <div class="mt-4 flex">
+                <img src="../assets/svg/facebook.svg" class="me-2"/>
+                <img src="../assets/svg/twitter.svg"  class="mx-2"/>
+                <img src="../assets/svg/dribble.svg"  class="mx-2"/>
+                <img src="../assets/svg/linkendIn.svg"  class="mx-2"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+</template>
+<script setup>
+</script>
+<style scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -349,33 +349,36 @@
             <div class="flex flex-col self-center">
               <div class="justify-self-start">
                 <h2 class="text-5xl font-extrabold">
-                  We Are <span class="text-[#F5CA56]">Loaft</span><br/> What our
-                  Priority <br/> <span class="text-[#F5CA56]">Client's Says</span>
+                  We Are <span class="text-[#F5CA56]">Loaft</span><br />
+                  What our Priority <br />
+                  <span class="text-[#F5CA56]">Client's Says</span>
                 </h2>
                 <p class="text-xl mt-6">
                   If you are looking at blank cassettes on the web, you may be
                   very <br />confused at the difference in price.
                 </p>
-                
-                <div class="mt-7">
-                  <carousel :items-to-show="1" wrapAround :autoplay="3000" >
-                    <slide v-for="slide in allData.items" :key="slide.id">
-                      <p class="carousel__item text-2xl text-start">{{ slide.name }}</p>
-                      <div class="carousel__item flex my-5 ">
-                        <div>
 
-                          <img src="../assets/testi-author.png"/>
+                <div class="mt-7">
+                  <carousel :items-to-show="1" wrapAround :autoplay="3000">
+                    <slide v-for="slide in allData.items" :key="slide.id">
+                      <p class="carousel__item text-2xl text-start">
+                        {{ slide.name }}
+                      </p>
+                      <div class="carousel__item flex my-5">
+                        <div>
+                          <img src="../assets/testi-author.png" />
                         </div>
                         <div class="ms-3">
                           <p class="font-extrabold text-start">Victory Ndu</p>
-                          <p class="text-gray-500 text-start">Managing Director</p>
-
+                          <p class="text-gray-500 text-start">
+                            Managing Director
+                          </p>
                         </div>
                       </div>
                     </slide>
-  
+
                     <template #addons>
-                       <!-- <navigation>
+                      <!-- <navigation>
                         <template #next>
                           <div class="carousel__next">
                             <img src="../assets/svg/move-right.svg" class="carousel__icon" />
@@ -387,27 +390,27 @@
                           </div>
                         </template>
                       </navigation>  -->
-                       <!-- <pagination />  -->
+                      <!-- <pagination />  -->
                     </template>
                   </carousel>
                 </div>
               </div>
-
             </div>
 
             <div>
-              <img src="../assets/testi-img.jpg"/>
+              <img src="../assets/testi-img.jpg" />
             </div>
           </div>
         </div>
       </div>
     </section>
+
+    <Footer />
   </div>
 </template>
 
 <script setup>
 import "vue3-carousel/dist/carousel.css"
-// import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel"
 
 const projectDoneStart = ref(0)
 const projectDoneEnd = ref(500)
@@ -470,14 +473,29 @@ function counter() {
 }
 
 const allData = reactive({
-    items: [
-    { id:  1, name: "I couldn't be happier with my recent purchase from this furniture website! The quality of the craftsmanship exceeded my expectations, and the customer service was exceptional. My new sofa has become the centerpiece of my living room, and I constantly receive compliments on its style and comfort." },
-    { id:  2, name: "Shopping for furniture online can be daunting, but this website made the process so easy and enjoyable. From browsing their wide selection to receiving my order ahead of schedule, every step was seamless. " },
-    { id:  3, name: "I've been searching for the perfect bedroom set for months, and I finally found it on this website! The variety of styles and finishes made it easy to find something that matched my taste and budget." },
-    { id:  4, name: "As an interior designer, I'm always on the lookout for unique pieces to elevate my clients' spaces, and this website has become my go-to resource. The curated collection of furniture and decor makes it easy to find pieces that fit any design aesthetic, from modern and minimalist to classic and traditional." },
-   
-  ]
+  items: [
+    {
+      id: 1,
+      name: "I couldn't be happier with my recent purchase from this furniture website! The quality of the craftsmanship exceeded my expectations, and the customer service was exceptional. My new sofa has become the centerpiece of my living room, and I constantly receive compliments on its style and comfort.",
+    },
+    {
+      id: 2,
+      name: "Shopping for furniture online can be daunting, but this website made the process so easy and enjoyable. From browsing their wide selection to receiving my order ahead of schedule, every step was seamless. ",
+    },
+    {
+      id: 3,
+      name: "I've been searching for the perfect bedroom set for months, and I finally found it on this website! The variety of styles and finishes made it easy to find something that matched my taste and budget.",
+    },
+    {
+      id: 4,
+      name: "As an interior designer, I'm always on the lookout for unique pieces to elevate my clients' spaces, and this website has become my go-to resource. The curated collection of furniture and decor makes it easy to find pieces that fit any design aesthetic, from modern and minimalist to classic and traditional.",
+    },
+  ],
 })
+
+const date = ref("")
+
+date.value = new Date().getFullYear()
 
 onMounted(() => {
   counter()
