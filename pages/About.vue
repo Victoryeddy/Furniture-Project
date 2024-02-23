0051f24d4b15ba87f836e75ad2733d6bf3465d15
Victory Ndu <victor5ndu@gmail.com>
2024-02-23 14:43:43 +0100

    Add about us

--- pages/About.vue
+++ pages/About.vue
@@ -1,15 +1,39 @@
 <template>
   <div>
-    Hello there , this is the about page
+    <section>
+      <div class="grid grid-cols-1 lg:grid-cols-3 gap-2 relative min-h-full">
+        <div class="col-span-2">
+          <img src="../assets/about-us.jpg" alt="" width="1000" />
+        </div>
+        <div class="ms-0 lg:ms-3 mt-10 px-3 lg:px-0">
+          <div class="flex flex-col justify-between">
+            <div class="flex flex-col mt-9">
+              <h1 class="text-5xl font-extrabold ms-10">About Us</h1>
+              <p class="ms-10 mt-7 font-medium text-xl capitalize">
+                we believe that furniture is more than just functional pieces;
+                it's an expression of your personality, style, and values.
+                
+              </p>
+            </div>
+          </div>
+          <div
+            class="bg-yellow-700 p-4 w-5/12 right-0 bottom-0 absolute hidden lg:block"
+          >
+            <p class="flex justify-center text-white hover-me">
+              <span class="mt-1 me-2 font-bold text-xl"
+                >Explore Now</span
+              >
+              <img src="../assets/svg/move-right.svg" class="" />
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <Testimonials/>
   </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-
-
-<style>
-
-</style>
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -338,70 +338,7 @@
       </div>
     </section>
 
-    <section>
-      <div class="py-[5rem]">
-        <div class="container mx-auto px-3">
-          <div
-            class="grid grid-cols-1 lg:grid-cols-2 gap-5 justify-items-stretch"
-          >
-            <div class="flex flex-col self-center">
-              <div class="justify-self-start">
-                <h2 class="text-5xl font-extrabold">
-                  We Are <span class="text-[#F5CA56]">Loaft</span><br />
-                  What our Priority <br />
-                  <span class="text-[#F5CA56]">Client's Says</span>
-                </h2>
-                <p class="text-xl mt-6">
-                  If you are looking at blank cassettes on the web, you may be
-                  very <br />confused at the difference in price.
-                </p>
-
-                <div class="mt-7">
-                  <carousel :items-to-show="1" wrapAround :autoplay="3000">
-                    <slide v-for="slide in allData.items" :key="slide.id">
-                      <p class="carousel__item text-2xl text-start">
-                        {{ slide.name }}
-                      </p>
-                      <div class="carousel__item flex my-5">
-                        <div>
-                          <img src="../assets/testi-author.png" />
-                        </div>
-                        <div class="ms-3">
-                          <p class="font-extrabold text-start">Victory Ndu</p>
-                          <p class="text-gray-500 text-start">
-                            Managing Director
-                          </p>
-                        </div>
-                      </div>
-                    </slide>
-
-                    <template #addons>
-                      <!-- <navigation>
-                        <template #next>
-                          <div class="carousel__next">
-                            <img src="../assets/svg/move-right.svg" class="carousel__icon" />
-                          </div>
-                        </template>
-                        <template #prev>
-                          <div class="carousel__prev relative top-[-5rem]">
-                            <img src="../assets/svg/move-right.svg"  class="carousel__icon"/>
-                          </div>
-                        </template>
-                      </navigation>  -->
-                      <!-- <pagination />  -->
-                    </template>
-                  </carousel>
-                </div>
-              </div>
-            </div>
-
-            <div>
-              <img src="../assets/testi-img.jpg" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+   <Testimonials/>
 
     <Footer />
   </div>
@@ -445,26 +382,7 @@ function counter() {
   }, 1)
 }
 
-const allData = reactive({
-  items: [
-    {
-      id: 1,
-      name: "I couldn't be happier with my recent purchase from this furniture website! The quality of the craftsmanship exceeded my expectations, and the customer service was exceptional. My new sofa has become the centerpiece of my living room, and I constantly receive compliments on its style and comfort.",
-    },
-    {
-      id: 2,
-      name: "Shopping for furniture online can be daunting, but this website made the process so easy and enjoyable. From browsing their wide selection to receiving my order ahead of schedule, every step was seamless. ",
-    },
-    {
-      id: 3,
-      name: "I've been searching for the perfect bedroom set for months, and I finally found it on this website! The variety of styles and finishes made it easy to find something that matched my taste and budget.",
-    },
-    {
-      id: 4,
-      name: "As an interior designer, I'm always on the lookout for unique pieces to elevate my clients' spaces, and this website has become my go-to resource. The curated collection of furniture and decor makes it easy to find pieces that fit any design aesthetic, from modern and minimalist to classic and traditional.",
-    },
-  ],
-})
+
 
 
 
