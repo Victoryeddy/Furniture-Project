dbffc7fa026f56fa2a89fa70af9b190ebc1e9bfe
Victory Ndu <victor5ndu@gmail.com>
2024-02-23 15:14:14 +0100

    complete about us

---
+++ components/Ratings.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="mt-5 grid grid-cols-3">
+      <div class="card p-3 m-2 flex justify-center items-center flex-col">
+        <img
+          src="../assets/svg/briefcase.svg"
+          class="icons-gallery"
+          width="60"
+        />
+
+        <p class="font-black text-3xl text-center mt-4">
+          {{ projectDoneStart }}
+        </p>
+        <p class="text-gray-500 font-bold">Projects Done</p>
+      </div>
+      <div class="card p-3 m-2 flex justify-center items-center flex-col">
+        <img src="../assets/svg/crown.svg" class="icons-gallery" width="60" />
+        <p class="font-black text-3xl text-center mt-4">
+          {{ membersStart }}
+        </p>
+        <p class="text-gray-500 font-bold">Expert Members</p>
+      </div>
+      <div class="card p-3 m-2 flex justify-center items-center flex-col">
+        <img src="../assets/svg/trophy.svg" class="icons-gallery" width="60" />
+        <p class="font-black text-3xl text-center mt-4">
+          {{ achievementStart }}
+        </p>
+        <p class="text-gray-500 font-bold">Achievements</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const projectDoneStart = ref(0)
+const projectDoneEnd = ref(500)
+
+const membersStart = ref(0)
+const membersEnd = ref(150)
+
+const achievementStart = ref(0)
+const achievementEnd = ref(225)
+
+function counter() {
+  const projectIntervalId = setInterval(() => {
+    if (projectDoneStart.value < projectDoneEnd.value) {
+      projectDoneStart.value++
+      // console.log(projectDoneStart.value)
+    } else {
+      clearInterval(projectIntervalId)
+    }
+  }, 1)
+  const memberIntervalId = setInterval(() => {
+    if (membersStart.value < membersEnd.value) {
+      membersStart.value++
+    } else {
+      clearInterval(memberIntervalId)
+    }
+  }, 1)
+  const achievementIntervalId = setInterval(() => {
+    if (achievementStart.value < achievementEnd.value) {
+      achievementStart.value++
+    } else {
+      clearInterval(achievementIntervalId)
+    }
+  }, 1)
+}
+
+onMounted(() => {
+  counter()
+})
+</script>
+
+<style></style>
--- pages/About.vue
+++ pages/About.vue
@@ -12,7 +12,6 @@
               <p class="ms-10 mt-7 font-medium text-xl capitalize">
                 we believe that furniture is more than just functional pieces;
                 it's an expression of your personality, style, and values.
-                
               </p>
             </div>
           </div>
@@ -20,9 +19,7 @@
             class="bg-yellow-700 p-4 w-5/12 right-0 bottom-0 absolute hidden lg:block"
           >
             <p class="flex justify-center text-white hover-me">
-              <span class="mt-1 me-2 font-bold text-xl"
-                >Explore Now</span
-              >
+              <span class="mt-1 me-2 font-bold text-xl">Explore Now</span>
               <img src="../assets/svg/move-right.svg" class="" />
             </p>
           </div>
@@ -30,10 +27,57 @@
       </div>
     </section>
 
-    <Testimonials/>
+    <section>
+      <div class="py-[5rem]">
+        <div class="container mx-auto px-3">
+          <div class="grid grid-cols-1 lg:grid-cols-2 gap-5">
+            <div>
+              <div class="">
+                <h2 class="text-5xl font-extrabold">
+                  Our <span class="text-[#F5CA56]">Achievement</span>
+                </h2>
+
+                <h2 class="text-4xl font-bold mt-6">Memorable Story</h2>
+                <p class="text-xl mt-4">
+                  Our journey began with a simple idea: to provide high-quality,
+                  stylish furniture that doesn't break the bank. We understand
+                  that furnishing your home is an investment, both financially
+                  and emotionally, which is why we're committed to offering
+                  affordable options without compromising on quality or design.
+                </p>
+                <p class="text-xl mt-4">
+                  But our mission goes beyond just selling furniture. We're here
+                  to inspire and empower you to create spaces that reflect your
+                  unique taste and lifestyle. Whether you're furnishing your
+                  first apartment, redesigning your home office, or simply
+                  looking for that perfect accent piece, we're here to help you
+                  every step of the way. Thank you for joining us on this
+                  journey. Together, let's transform houses into homes and turn
+                  living spaces into expressions of artistry and individuality.
+                </p>
+                <p class="mt-5">
+                  <img src="../assets/svg/move-right (1).svg" />
+                </p>
+              </div>
+              <Ratings />
+            </div>
+            <div class="flex justify-center">
+              <img src="../assets/SPS_1374-removebg-preview.png" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <Testimonials />
+    <Footer />
   </div>
 </template>
 
 <script setup></script>
 
-<style></style>
+<style scoped>
+* {
+  outline: 1px solid red;
+}
+</style>
