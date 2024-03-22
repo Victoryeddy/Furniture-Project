9776ad0b9d55b85f35ec098272e4411061e8f955
Victory Ndu <victor5ndu@gmail.com>
2024-03-22 21:33:20 +0100

    Adjust width of navbar

--- components/Aside.vue
+++ components/Aside.vue
@@ -1,16 +1,48 @@
 <template>
-  <div class="right-0 bg-[#f9f9ff] w-[30%] p-5  z-30 top-0 fixed h-full">
-     <ul class="mt-[8rem] p-5 text-center">
-      <li class="my-4"><NuxtLink :to="{name: 'index'}" class="font-extrabold hover:border-b-2 border-[#A1621F]">Home</NuxtLink></li>
-      <li class="my-4"><NuxtLink to="/about" class="font-extrabold hover:border-b-2 border-[#A1621F]">About</NuxtLink></li>
+  <div class="right-0 bg-[#f9f9ff] p-5 z-30 top-0 fixed h-full nav">
+    <ul class="mt-[8rem] p-5 text-center">
+      <li class="my-4">
+        <NuxtLink
+          :to="{ name: 'index' }"
+          class="font-extrabold hover:border-b-2 border-[#A1621F]"
+          >Home</NuxtLink
+        >
+      </li>
+      <li class="my-4">
+        <NuxtLink
+          to="/about"
+          class="font-extrabold hover:border-b-2 border-[#A1621F]"
+          >About</NuxtLink
+        >
+      </li>
       <!-- <li class="my-4"><NuxtLink to="#" class="font-extrabold hover:border-b-2 border-[#A1621F]">Work</NuxtLink></li> -->
-      <li class="my-4"><NuxtLink to="/blogs" class="font-extrabold hover:border-b-2 border-[#A1621F]">Blogs</NuxtLink></li>
-      <li class="my-4"><NuxtLink to="/contact" class="font-extrabold hover:border-b-2 border-[#A1621F]">Contact</NuxtLink></li>
-     </ul>
+      <li class="my-4">
+        <NuxtLink
+          to="/blogs"
+          class="font-extrabold hover:border-b-2 border-[#A1621F]"
+          >Blogs</NuxtLink
+        >
+      </li>
+      <li class="my-4">
+        <NuxtLink
+          to="/contact"
+          class="font-extrabold hover:border-b-2 border-[#A1621F]"
+          >Contact</NuxtLink
+        >
+      </li>
+    </ul>
   </div>
 </template>
 
-<script setup>
+<script setup></script>
+<style>
+.nav {
+  width: 40%;
+}
 
-</script>
-
+@media only screen and (max-width: 40rem) {
+  .nav {
+    width: 50%;
+  }
+}
+</style>
