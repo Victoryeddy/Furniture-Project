d02aa48737a88a81ef976720289214db973c7199
Victory Ndu <victor5ndu@gmail.com>
2024-03-22 21:27:09 +0100

    Add width to asidebar

--- components/Aside.vue
+++ components/Aside.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="right-0 bg-[#f9f9ff] w-[20%] p-5  z-30 top-0 fixed h-full">
+  <div class="right-0 bg-[#f9f9ff] w-[30%] p-5  z-30 top-0 fixed h-full">
      <ul class="mt-[8rem] p-5 text-center">
       <li class="my-4"><NuxtLink :to="{name: 'index'}" class="font-extrabold hover:border-b-2 border-[#A1621F]">Home</NuxtLink></li>
       <li class="my-4"><NuxtLink to="/about" class="font-extrabold hover:border-b-2 border-[#A1621F]">About</NuxtLink></li>
